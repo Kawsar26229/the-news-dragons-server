4a8b7f4f4e68731a94669f1f947fd647b8902968
Kawsar26229 <knkawsar26229@gmail.com>
2023-11-29 01:57:14 +0600

    final

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
 
 const categories = require("./data/categories.json");
 const news = require("./data/news.json");
+
+app.use(cors());
 
 app.get("/", (_req, res) => {
   res.send("Dragon is running...");
