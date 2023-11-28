28cf6980c178aeec313bdf21331216c57fdb002a
Kawsar26229 <knkawsar26229@gmail.com>
2023-11-29 01:25:30 +0600

    final commit

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-const categories = require('./data/categories.json');
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
-app.get('/', (req, res) => {
-    res.send('Dragon is running...')
-})
+app.get("/", (_req, res) => {
+  res.send("Dragon is running...");
+});
 
-app.get('/categories', (req, res) => {
-    res.send(categories)
-})
+app.get("/categories", (_req, res) => {
+  res.send(categories);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === '0') {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news", (_req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
